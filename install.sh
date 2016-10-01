7d1d9caf0a32317e3189f0036c3b2f8b870e3dcd
Kwon-Young <kwon-young.choi@hotmail.fr>
2016-10-01 14:50:16 +0200

    added spell files installation

--- install.sh
+++ install.sh
@@ -38,6 +38,7 @@ ln -sf "$(readlink -e "init.vim")" $HOME/.vim/init.vim
 ln -sf "$(readlink -e "ginit.vim")" $HOME/.vim/ginit.vim
 ln -sf "$(readlink -e "personal_config")" $HOME/.vim/personal_config
 ln -sf "$(readlink -e "plug_config")" $HOME/.vim/plug_config
+ln -sf "$(readlink -e "spell")" $HOME/.vim/spell
 
 # For Neovim
 mkdir -p $HOME/.local/share/nvim/swp
@@ -45,3 +46,11 @@ mkdir -p $HOME/.local/share/nvim/swp
 ln -sf $HOME/.vim $HOME/.config/nvim
 ln -sf $HOME/.config/nvim/backup $HOME/.local/share/nvim/backup
 ln -sf $HOME/.config/nvim/undo $HOME/.local/share/nvim/undo
+
+# Installing fonts
+mkdir -p $HOME/.fonts
+ln -sf "$(readlink -e "./fonts/Inconsolata Bold for Powerline.ttf")" \
+  "$HOME/.fonts/Inconsolata Bold for Powerline.otf"
+ln -sf "$(readlink -e "./fonts/Inconsolata for Powerline.otf")" \
+  "$HOME/.fonts/Inconsolata for Powerline.otf"
+fc-cache -fv
